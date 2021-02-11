36873f04099fd29641021501c0dc3df36fa0a774
Sampath Liyanage <plbsam@gmail.com>
2021-02-12 01:15:17 +0530

    config service for configurations

---
+++ src/app/config.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+
+  constructor() { }
+
+  public static readonly SERVER_API_URL = 'http://127.0.0.1:3000/schools';
+  public static readonly NO_RECORDS_PER_PAGE = 10;
+}
